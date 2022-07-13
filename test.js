e3faf2eead2acb190c7002801c7ad77e82e556ab
Tyler Wellman <TangoKilo1985@gmail.com>
2022-07-13 08:00:27 -0400

    Gitz

---
+++ test.js
@@ -0,0 +1,31 @@
+function getAdd (a,b) {
+    var add = (a + b);
+    console.log(add);
+
+}
+
+getAdd(2,3);
+
+function getSubtract (c,d) {
+    var subtract = (c - d);
+    console.log(subtract);
+
+}
+
+getSubtract(7,4);
+
+function getMultiply (e,f) {
+    var multiply = (e * f);
+    console.log(multiply);
+
+}
+
+getMultiply(4,4);
+
+function getDivide (g,h) {
+    var divide = (g / h);
+    console.log(divide);
+
+}
+
+getDivide(20,10);
